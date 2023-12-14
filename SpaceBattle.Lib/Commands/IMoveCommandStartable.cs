3fe451fe78835c47108ebceaedf0b543460b7acb
egashok <55044715+Egashok@users.noreply.github.com>
2023-12-14 12:28:13 +0600

    Realize StartMoveCommand

---
+++ SpaceBattle.Lib/Commands/IMoveCommandStartable.cs
@@ -0,0 +1,8 @@
+﻿namespace SpaceBattle.Lib;
+
+public interface IMoveCommandStartable
+{
+    public IUObject UObject { get; }
+    public IQueue Queue { get; }
+    public IDictionary<string, object> Parameters { get; }
+}
