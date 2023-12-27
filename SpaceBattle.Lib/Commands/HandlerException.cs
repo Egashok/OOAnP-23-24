c5629b1f2251031e9b1fae65d37ec7c68f3b0907
egashok <55044715+Egashok@users.noreply.github.com>
2023-12-28 00:42:28 +0600

    added tests

--- SpaceBattle.Lib/Commands/HandlerException.cs
+++ SpaceBattle.Lib/Commands/HandlerException.cs
@@ -2,35 +2,37 @@
 
 namespace SpaceBattle.Lib
 {
-    public class HandlerException:IHandler
+    public class HandlerException : IHandler
     {
-        private  Type _commandType;
-        private  Type _exceptionType;
+        private Type _commandType;
+        private Type _exceptionType;
 
         public ICommand SearchHandler(params Type[] args)
         {
-        //Получаем типы для поиска обработчика
-            _commandType =  args[0];
+            //Получаем типы для поиска обработчика
+            _commandType = args[0];
             _exceptionType = args[1];
+
             //Дерево с типом комманды и с объектом (обработчик или <тип ошибки,обработчик> )
             var tree = IoC.Resolve<Dictionary<Type, object>>("Commands.Tree");
 
-            // Пытаемся получить обработчик по комманде либо словарь <ошибка,обработчик> либо обработчик
+            // Пытаемся получить либо словарь <ошибка,обработчик> либо обработчик
             var subtree = tree.GetValueOrDefault(_commandType, tree.GetValueOrDefault(_exceptionType, IoC.Resolve<ICommand>("Commands.Handler")));
 
             //если получен только обработчик
             if (subtree.GetType() != typeof(Dictionary<Type, object>))
             {
                 //преобразуем обработчик в айкомманд и возвращаем его
-                var handler = (ICommand) subtree;
+                var handler = (ICommand)subtree;
                 return handler;
-            }   
+            }
             // если получен словарь из ошибок и обработчиков
             else
-            {   
+            {
                 //берем словарь ошибок и обработчиков
                 var exTree = (Dictionary<Type, object>)subtree;
-                //и получаем обработчик уже для словаря ошибок 
+
+                //если нет обработчика для ошибок то получаем дефолтный для команды из айока
                 var handler = (ICommand)exTree.GetValueOrDefault(_exceptionType, IoC.Resolve<ICommand>("Commands.Handler", _commandType));
                 return handler;
             }
