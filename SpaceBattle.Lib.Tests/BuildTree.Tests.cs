565591bfd7a683005e3ea81fd7696ca15b74408c
egashok <55044715+Egashok@users.noreply.github.com>
2023-12-28 17:55:43 +0600

    realize collisionTree (#3)
    * realize collisionTree
    
    * change yml

---
+++ SpaceBattle.Lib.Tests/BuildTree.Tests.cs
@@ -0,0 +1,59 @@
+﻿namespace SpaceBattle.Lib.Test;
+using Hwdtech;
+using Hwdtech.Ioc;
+using Moq;
+
+public class CollisionTreeBuildTest
+{
+    public CollisionTreeBuildTest()
+    {
+        new InitScopeBasedIoCImplementationCommand().Execute();
+
+        IoC.Resolve<ICommand>("Scopes.Current.Set",
+            IoC.Resolve<object>("Scopes.New",
+                IoC.Resolve<object>("Scopes.Root"))
+        ).Execute();
+    }
+    [Fact]
+    public void NegativeBuildCollisionTreeNoneFile()
+    {
+
+        var mockCollisionTreeBuild = new CollisionTreeBuilder();
+
+        IoC.Resolve<ICommand>("IoC.Register",
+            "Collision.BuildTree", (object[] args) => mockCollisionTreeBuild).Execute();
+
+        var TestCollisionTreeCommand = new BuildCollision("none");
+
+        Assert.Throws<FileNotFoundException>(() => TestCollisionTreeCommand.Execute());
+    }
+
+    [Fact]
+    public void PositiveBuildCollisionTree()
+    {
+        var tree = new Dictionary<int, object>();
+        IoC.Resolve<ICommand>("IoC.Register",
+            "Collision.CollisionTree", (object[] args) => tree).Execute();
+
+        var mockCollisionTreeBuild = new CollisionTreeBuilder();
+        mockCollisionTreeBuild.BuildArrayFile("../../../Data.txt");
+
+        Assert.Equal(2, IoC.Resolve<IDictionary<int, object>>("Collision.CollisionTree").Count);
+        Assert.Equal(2, ((IDictionary<int, object>)IoC.Resolve<IDictionary<int, object>>("Collision.CollisionTree")[1]).Count);
+    }
+    [Fact]
+    public void PositiveReadBuildCollisionTree()
+    {
+        var mockCollisionTreeBuild = new Mock<ICollisionTreeBuild>();
+        mockCollisionTreeBuild.Setup(x => x.BuildArrayFile(It.IsAny<string>())).Verifiable();
+
+        IoC.Resolve<ICommand>("IoC.Register",
+            "Collision.BuildTree", (object[] args) => mockCollisionTreeBuild.Object).Execute();
+
+        var TestCollisionTreeCommand = new BuildCollision("any");
+
+        TestCollisionTreeCommand.Execute();
+
+        mockCollisionTreeBuild.Verify(x => x.BuildArrayFile("any"), Times.Once());
+    }
+}
---
+++ SpaceBattle.Lib/Commands/CollisionTreeBuildCommand.cs
@@ -0,0 +1,24 @@
+﻿namespace SpaceBattle.Lib;
+using Hwdtech;
+
+public class CollisionTreeBuilder : ICollisionTreeBuild
+{
+    private static IEnumerable<IEnumerable<int>> ReadArrayFile(string dataPath)
+    {
+        return File.ReadAllLines(dataPath).Select(line => line.Split().Select(int.Parse));
+    }
+
+    private static void BuildTree(IEnumerable<IEnumerable<int>> dataFile)
+    {
+        dataFile.ToList().ForEach(line =>
+        {
+            var node = IoC.Resolve<IDictionary<int, object>>("Collision.CollisionTree");
+            line.ToList().ForEach(n =>
+            {
+                node.TryAdd(n, new Dictionary<int, object>());
+                node = (Dictionary<int, object>)node[n];
+            });
+        });
+    }
+    public void BuildArrayFile(string path) => BuildTree(ReadArrayFile(path));
+}
