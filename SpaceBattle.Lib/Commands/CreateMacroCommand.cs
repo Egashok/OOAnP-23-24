e573f84d5a4740714d75a68e98308f7d56292120
egashok <55044715+Egashok@users.noreply.github.com>
2023-12-28 20:53:28 +0600

    Macro command (#4)
    * Added macrocommand
    
    * fix macro and added test

---
+++ SpaceBattle.Lib/Commands/CreateMacroCommand.cs
@@ -0,0 +1,24 @@
+﻿using Hwdtech;
+
+public class CreateMacroCommand
+{
+    private readonly string _commandsNames;
+
+    public CreateMacroCommand(string commandsNames)
+    {
+        _commandsNames = commandsNames;
+    }
+
+    public ICommand[] SeparationCommand()
+    {
+        var commandList = new List<ICommand>();
+        var commandArrayNames = IoC.Resolve<string[]>(_commandsNames);
+
+        foreach (var commandName in commandArrayNames)
+        {
+            commandList.Add(IoC.Resolve<ICommand>(commandName));
+        }
+
+        return commandList.ToArray();
+    }
+}
---
+++ SpaceBattle.Lib/Commands/MacroCommand.cs
@@ -0,0 +1,14 @@
+﻿using Hwdtech;
+public class MacroCommand : ICommand
+{
+    private readonly IList<ICommand> _commands;
+
+    public MacroCommand(IList<ICommand> commands)
+    {
+
+        _commands = commands;
+    }
+    public void Execute() => _commands.ToList().ForEach(
+        command => command.Execute()
+    );
+}
